f99b55a019b3fb74d306f9898cd4b77b1965c52a
Lil <bittarlily@gmail.com>
2021-10-21 23:15:51 +0200

    solution 4 from lecture 1

---
+++ Lecture1/PrintingNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrintingNumbers
+{
+    class Program {
+
+        static void PrintEven(int x) {
+            for (int i = 0; i < x; i++) {
+                if (i % 2 == 0) {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintOdd(int x) {
+            for (int i = 0; i < x; i++) {
+                if (i % 2 != 0) {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine();
+        }
+		
+        static void PrintDivisible(int x, int y) {
+            for (int i = 0; i < x; i++) {
+                if (i % y == 0) {
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) {
+            PrintEven(10);
+            PrintOdd(10);
+            PrintDivisible(7, 4);
+        }
+    }
+}
