01dfd4c51be977e7a76734b757dc0d033c64cd37
Lil <bittarlily@gmail.com>
2021-10-21 23:49:38 +0200

    solution ex.10 from lecture 2

---
+++ Lecture2/GameQuestLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GameQuestLogic
+{
+    class Program
+    {
+        static bool CanFastAttack(bool isKnightAwake){
+            return isKnightAwake;
+        }
+        static bool CanSpy(bool isKnightAwake, bool archerIsAwake){
+            return isKnightAwake || archerIsAwake;
+        }
+        static bool CanSignal(bool IsPresenerAwake, bool archerIsAwake){
+            return IsPresenerAwake && !archerIsAwake;
+        }
+        static bool CanFree(bool IsPresenerAwake, bool isKnightAwake, bool archerIsAwake, bool hasDog){
+            return (IsPresenerAwake && !isKnightAwake && !archerIsAwake) || (IsPresenerAwake && !archerIsAwake && isKnightAwake && hasDog);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanFree(true,true,false,true));
+        }
+    }
+}
