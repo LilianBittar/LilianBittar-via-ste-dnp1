ba6e47434db7d66fc6a1d554b154204b59d8d674
Lil <bittarlily@gmail.com>
2021-10-21 23:25:26 +0200

    solution ex.9 from lecture 1

---
+++ Lecture1/Substring/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program {
+
+        static string NTwice(string x, int n) {
+            return $"{x.Substring(0, n)}{x.Substring(x.Length - n, n)}";
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(NTwice("Hello", 2));
+            Console.WriteLine(NTwice("Chocolate", 3));
+            Console.WriteLine(NTwice("Chocolate", 1));
+        }
+    }
+}
