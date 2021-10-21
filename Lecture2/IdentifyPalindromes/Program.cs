9b0efade24b9a3077f9e944be180c78f3fac2d39
Lil <bittarlily@gmail.com>
2021-10-21 23:46:26 +0200

    solution ex.7 from lecture 2

---
+++ Lecture2/IdentifyPalindromes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IdentifyPalindromes
+{
+    class Program
+    {
+        static bool IsPalindrome(string x){
+            for(int i = 0; i < x.Length; i++) if(x[i] == ' ')x = x.Remove(i,1);
+            for(int i = 0; i < x.Length/2; i++) if(x[i] != x[x.Length - i -1]) return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("mr owl ate my metal worm"));
+        }
+    }
+}
