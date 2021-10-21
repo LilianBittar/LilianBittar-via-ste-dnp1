264cbe18c34fd02b63f8b47111739b9e1936ca1d
Lil <bittarlily@gmail.com>
2021-10-21 23:18:38 +0200

    solution ex.5 from lecture 1

---
+++ Lecture1/MathInSeparateNamespace/MathLib/MathLib.cs
@@ -0,0 +1,19 @@
+﻿namespace MathInSeparateNamespace.MathLib
+{
+    public class MathLib
+    {
+        class Calculator{
+            public int Add(int a, int b) {
+                return a + b;
+            }
+
+            public int Add(int[] arr) {
+                int sum = 0;
+                foreach (int i in arr) {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+    }
+}
---
+++ Lecture1/MathInSeparateNamespace/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MathInSeparateNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculator();
+            Console.WriteLine(c.Add(4,2));
+            Console.WriteLine(c.Add(new int[] {4, 8, 1, 3}));
+        }
+    }
+}
