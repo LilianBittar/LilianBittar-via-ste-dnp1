8b7bbefcaafbee3167f4bb59203097b796aded66
Lil <bittarlily@gmail.com>
2021-10-22 00:31:53 +0200

    solution Blazor from lecture 4

---
+++ Lecture4/BlazorTutorial/Data/ITodoData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BlazorTutorial.Models;
+
+namespace BlazorTutorial.Data
+{
+    public interface ITodoData
+    {
+        IList<Todo> GetTodos();
+        void AddTodo(Todo todo);
+        void RemoveTodo(int todoId);
+        void Update(Todo todo);
+        Todo Get(int id);
+
+    }
+    
+}
