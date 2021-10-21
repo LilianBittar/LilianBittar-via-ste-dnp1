9b25627f6b64a58c4798a4a0a710a1bbce8c0995
Lil <bittarlily@gmail.com>
2021-10-21 23:24:06 +0200

    solution ex.8 from lecture 1

---
+++ Lecture1/SurroundWith/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SurroundWith
+{
+    class Program {
+
+        static string MakeOutWord(string outer, string inner) {
+            return $"{outer.Substring(0, outer.Length / 2)}{inner}{outer.Substring(outer.Length / 2, outer.Length / 2)}";
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(MakeOutWord("<<>>", "Yay"));
+            Console.WriteLine(MakeOutWord("<<>>", "WooHoo"));
+            Console.WriteLine(MakeOutWord("[[]]", "word"));
+        }
+    }
+}
