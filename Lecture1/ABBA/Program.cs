d4b90b50679d64217137a4cbee03b807ae37a463
Lil <bittarlily@gmail.com>
2021-10-21 23:23:06 +0200

    solution ex.7 from lecture 1

---
+++ Lecture1/ABBA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ABBA
+{
+    class Program {
+
+        static string MakeABBA(string a, string b) {
+            return $"{a}{b}{b}{a}";
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(MakeABBA("Hi", "Bye"));
+            Console.WriteLine(MakeABBA("Yo", "Alice"));
+            Console.WriteLine(MakeABBA("What", "Up"));
+        }
+    }
+}
