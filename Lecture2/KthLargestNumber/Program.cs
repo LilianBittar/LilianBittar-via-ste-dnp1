1c97ac1ba1378e588cc0fb2882a85acd1cf22828
Lil <bittarlily@gmail.com>
2021-10-21 23:47:37 +0200

    solution ex.8 from lecture 2

---
+++ Lecture2/KthLargestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KthLargestNumber
+{
+    class Program
+    {
+        static int Largest(int[] ints, int k){
+            if(k > ints.Length) throw new Exception("K is larger than the length of the array");
+            for(int i = 0; i < ints.Length -1; i++){
+                for(int j = 0; j < ints.Length -1 -i; j++) if(ints[j] > ints[j + 1]){
+                    int temp = ints[j + 1];
+                    ints[j + 1] = ints[j];
+                    ints[j] = temp;
+                }
+            }
+            return ints[ints.Length - k];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Largest(new int[] {6, 3, 5, 9,1, 2}, 1));
+        }
+    }
+}
