f13289c8d8b357392b574aa2d4fcfa2871470fe7
Lil <bittarlily@gmail.com>
2021-10-22 00:05:16 +0200

    solution ex.2 from lecture 3

---
+++ Lecture3/JSONFileStorage/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace JSONFileStorage
+{
+    class Person
+    {
+        public string FirstName {get; set; }
+        public string LastName {get; set; }
+        public int Age {get; set;}
+        public double Height {get; set;}
+        public bool IsMarried {get; set; }
+        public char Sex {get; set; }
+        public string[] Hobbies {get; set;}   
+    }
+}
---
+++ Lecture3/JSONFileStorage/PersonPersistence.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace JSONFileStorage
+{
+    class PersonPersistance
+    {
+        public static void StoreObject(List<Person> persons){
+            string personsAsJsonString = JsonSerializer.Serialize(persons, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+            File.WriteAllText("exmple.json", personsAsJsonString);
+        }
+
+        public static string ReadJSON(string FileName){
+            return System.IO.File.ReadAllText(FileName);
+        }    
+    }
+}
