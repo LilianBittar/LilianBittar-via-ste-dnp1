8cb790c0c953538b3f4c7027698d195e25936994
Lil <bittarlily@gmail.com>
2021-10-22 00:45:36 +0200

    solution FourthCoffee from lecture 4

---
+++ Lecture4/FourthCoffee/Data/IProductsService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FourthCoffee.Model;
+
+namespace FourthCoffee.Data
+{
+    public interface IProductsService
+    {
+        public IList<Product> Products { get; }
+    }
+}
