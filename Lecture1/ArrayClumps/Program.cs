27574d03cc6cb7d213de21a91df8f9e13eb1b81a
Lil <bittarlily@gmail.com>
2021-10-21 23:13:03 +0200

    solution ex 2-3 from lecture 1

---
+++ Lecture1/ArrayClumps/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayClumps
+{
+    class Program {
+
+        static int CountClumps(int[] arr) {
+            int clumps = 0;
+            bool inClump = false;
+            int i = 1;
+            while (i < arr.Length) {
+                if (arr[i - 1] == arr[i]) {
+                    if (!inClump) {
+                        inClump = true;
+                        clumps++;
+                    }
+                } else {
+                    inClump = false;
+                }
+                i++;                
+            }
+            return clumps;
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine(CountClumps(new int[] {1, 2, 2, 3, 4, 4}));
+            Console.WriteLine(CountClumps(new int[] {1, 1, 2, 1, 1}));
+            Console.WriteLine(CountClumps(new int[] {1, 1, 1, 1, 1}));
+        }
+    }
+}
---
+++ Lecture1/SimplePerson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimplePerson
+{
+    class Person
+    {
+        private string name;
+        public void Introduce(){
+            Console.WriteLine($"Hi, I am {name}");
+        }
+        static void Main(string[] args)
+        {
+            Person p = new(){
+                name = "Lilian"
+            };
+            p.Introduce();
+
+        }
+    }
+}
